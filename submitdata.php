32783f249b2179c9387914f746df481b33b2b11a
Tuanminh batch developer <89622068+TuanminhDev@users.noreply.github.com>
2021-10-02 10:32:03 +0700

    Update submitdata.php

--- submitdata.php
+++ submitdata.php
@@ -1,10 +1,10 @@
-<!DOCTYPE html>
 <html>
-<meta charset="utf-8">
-<title>Forms action page</title>
-<link rel="stylesheet" href="/w3css/4/w3.css">
-<body class="w3-container">
-<h1>Submitted Form Data</h1>
-<h2>Your input was received as:</h2>
-<div class="w3-container w3-large w3-border" style="word-wrap:break-word">
-&nbsp;</div>
+<body>
+
+Hello <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+<br>
+<p> We see you! man </p>
+
+</body>
+</html>
